28ff12192ac9dcf2cb3792c8a9b420799036c386
alpha-tango <atpope5@gmail.com>
2017-02-19 16:12:02 -0500

    + first templates and bootstrap

--- microlives/views.py
+++ microlives/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Microlife home.")
+    context = {}
+    return render(request, 'microlives/index.html', context)
