72cf0b915fa20a898d914df3e9f01c9d8690049e
alpha-tango <atpope5@gmail.com>
2017-02-19 15:39:59 -0500

    + home and admin

--- website/settings.py
+++ website/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'microlives.apps.MicrolivesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
