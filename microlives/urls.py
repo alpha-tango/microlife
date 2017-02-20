3580a48bb35e29144909a2669f57f54386870c34
alpha-tango <atpope5@gmail.com>
2017-02-19 20:00:25 -0500

    + logins

--- microlives/urls.py
+++ microlives/urls.py
@@ -4,5 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^', include('registration.backends.simple.urls')),
 ]
