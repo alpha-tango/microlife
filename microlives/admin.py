7c29d40d769f7aa03b196816392392e71cf1926c
alpha-tango <atpope5@gmail.com>
2017-02-21 21:58:52 -0500

    + more admin fun

--- microlives/admin.py
+++ microlives/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from models import SiteUser, MicroLife
+from models import SiteUser, MicroLife, Exposure
 
 admin.site.register(SiteUser)
 admin.site.register(MicroLife)
+admin.site.register(Exposure)
