2efe7eafd0bdb55d1279ca701fd1f973a82b33f1
alpha-tango <atpope5@gmail.com>
2017-02-21 21:56:09 -0500

    + initial user profile

--- microlives/views.py
+++ microlives/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.detail import DetailView
+from microlives.models import MicroLife, Exposure, SiteUser
+from django.contrib.auth.models import User
 
 def index(request):
     context = {}
     return render(request, 'microlives/index.html', context)
+
+class UserProfileView(DetailView):
+    model = User
+    slug_field = "username"
+    template_name = "microlives/user_profile.html"
