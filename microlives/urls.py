b9a450e8122a46ae0ce49ede840c66ebe6c05b76
alpha-tango <atpope5@gmail.com>
2017-02-21 21:59:00 -0500

    ordering. sigh

--- microlives/urls.py
+++ microlives/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<slug>[\w.@+-]+)/$', views.UserProfileView.as_view()),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^', include('registration.backends.simple.urls')),
+    url(r'^(?P<slug>[\w.@+-]+)/$', views.UserProfileView.as_view()),
 ]
